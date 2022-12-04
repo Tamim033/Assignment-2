18d6349c1b392a3b9b3e368572ff82bba0a14dd7
Tamim033 <sami.firozi@gmail.com>
2022-12-04 21:14:15 +0600

    update

--- Assignment-1.py
+++ Assignment-1.py
@@ -5,10 +5,12 @@ for a in range (len(list_1)):
     list_3.append(int(list_1[a]) + int(list_2[a]))
 print("Addition of the Lists :", list_3)
 print("Reverse List:", list_3[::-1])
-list_3.insert(0,9)
-print("Added in Beginning-", list_3 )
+i1= input("Plz input after which item need to add 9 :")
+i2= list_3.index(int(i1))
+list_3.insert(i2+1,9)
+print("Added List", list_3 )
 list_4 = [5, 20, 15, 20, 25, 50, 20]
-print("Repeated item list :", list_4)
-item = input('Plz input item to remove from new list: ')
+item = 20
+print("Repeated item list : ", list_4 , "item to be remove:", item )
 list_4 = [i for i in list_4 if i != int(item)]
 print("Removed List :", list_4)
