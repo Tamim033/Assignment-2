a1b8d626aa9fd605f80325f6ddd06b53cc2f8741
Tamim <119634267+Tamim033@users.noreply.github.com>
2022-12-11 16:33:01 +0600

    Update P1_Vpark.py
    A

--- P1_Vpark.py
+++ P1_Vpark.py
@@ -19,4 +19,7 @@ Bike_Charge = input("please Enter Bike Parking Charge/Hour:  ")
 
 #**************************************Initial Input End ****************************************
 #**************************************Vehicle Input*********************************************
-
+vtype_input = input("please input Vehicle Type: C= for Car or  M= for Motorbike ")
+vnumber_input = input("please input Vehicle Number: ex: DM-KA-**-****  ")
+Mobile_N_input = input("please input MO #: ex: 01712-814295 ")
+in_time = input ("please input In Time #: ex: 13:20 ")
