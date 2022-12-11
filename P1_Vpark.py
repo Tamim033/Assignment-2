067e90cbff3ec43109d6174c2617f39fe6eba432
Tamim033 <sami.firozi@gmail.com>
2022-12-11 13:40:49 +0600

    P1_Vpark

--- P1_Vpark.py
+++ P1_Vpark.py
@@ -2,16 +2,21 @@
 
 #"*************************************Initial Inputs First Time***********************************"
 
-Car_input = input("please Enter Total Car Parking Spaces:  ")
-Mbike_input = input("please Enter Total Motorbike Parking Spaces: ")
-print ('Car parking Total :', int (Car_input) , sep= '')
-print ('Motorbike parking Total :', int (Mbike_input) , sep= '')
+Car_input = int(input("please Enter Total Car Parking Spaces:  "))
+C_park = []
+for c in range(Car_input):
+     C_park.append(c+1)
+print ('Car parking Spaces:', C_park, sep= ' ')
+
+Mbike_input = int (input("please Enter Total Motorbike Parking Spaces: "))
+M_park = []
+for b in range(Mbike_input):
+     M_park.append(b+1)
+print ('Motorbike parking Space:', M_park, sep= '')
+
 Car_Charge = input("please Enter Car Parking Chrge/Hour:  ")
 Bike_Charge = input("please Enter Bike Parking Charge/Hour:  ")
-print ('Car parking Charge Per Hour :', int (Car_Charge) , sep= '')
-print ('Motorbike parking Charge per Hour :', int (Bike_Charge) , sep= '')
 
-
-#**************************************Initial Input End *******************************************
-
+#**************************************Initial Input End ****************************************
+#**************************************Vehicle Input*********************************************
 
