94aa687ed183d912a3e71e4ca70335541be4d4be
Tamim033 <sami.firozi@gmail.com>
2022-12-11 11:40:13 +0600

    P1_Vpark

---
+++ P1_Vpark.py
@@ -0,0 +1,16 @@
+#"*******************************Vpark-Vehicle Parking Management***********************"
+
+#"*************************************Initial Inputs First Time***********************************"
+
+Car_input = input("please Enter Total Car Parking Spaces:  ")
+Mbike_input = input("please Enter Total Motorbike Parking Spaces: ")
+print ('Car parking Total :', int (Car_input) , sep= '')
+print ('Motorbike parking Total :', int (Mbike_input) , sep= '')
+Car_Charge = input("please Enter Car Parking Chrge/Hour:  ")
+Bike_Charge = input("please Enter Bike Parking Charge/Hour:  ")
+print ('Car parking Charge Per Hour :', int (Car_Charge) , sep= '')
+print ('Motorbike parking Charge per Hour :', int (Bike_Charge) , sep= '')
+
+
+#**************************************Initial Input End *******************************************
+
